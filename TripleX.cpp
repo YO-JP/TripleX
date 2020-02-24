db6499a3c23012a6848d9babf3e49cec794e0aad
Yuta Obara <o.yuta722@gmail.com>
2020-02-23 22:38:46 -0500

    added sum/products

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
-    std:: cout << "Hello, World!";
+    //Print welcome messages to the terminal
+    std::cout << "You are a hacker breaking into NASA's secure server";
+    std::cout << std::endl;
+    std::cout << "Enter the correct security password to continue..." << std::endl;
+
+    
+    //Declare 3 number code
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    /*
+    Declare sum and product variable
+    */
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    //Print sumb and product to the terminal    
+    std::cout << std::endl;
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The codes add-up to: " << sum << std::endl;
+    std::cout << "The codes multiply to give: " << product << std::endl;
+
     return 0;
 }
