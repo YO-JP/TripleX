75ffa39318b481b96331e861f0cb2f134d663037
Yuta Obara <o.yuta722@gmail.com>
2020-02-23 23:23:54 -0500

    endl -> \n & cin & if/else

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,28 +3,43 @@
 int main()
 {
     //Print welcome messages to the terminal
-    std::cout << "You are a hacker breaking into NASA's secure server";
-    std::cout << std::endl;
-    std::cout << "Enter the correct security password to continue..." << std::endl;
+    std::cout << "You are a hacker breaking into NASA's secure server\n";
+    std::cout << "Enter the correct security password to continue...\n\n";
 
     
     //Declare 3 number code
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
 
     /*
     Declare sum and product variable
     */
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    //Print sumb and product to the terminal    
+    //Print sum and product to the terminal    
     std::cout << std::endl;
-    std::cout << "There are 3 numbers in the code" << std::endl;
-    std::cout << "The codes add-up to: " << sum << std::endl;
-    std::cout << "The codes multiply to give: " << product << std::endl;
+    std::cout << "There are 3 numbers in the code\n";
+    std::cout << "The codes add-up to: " << CodeSum;
+    std::cout << "\nThe codes multiply to give: " << CodeProduct;
 
+    //Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\nHack: Successful";
+    }
+    else
+    {
+        std::cout << "\nDetected! Run!";
+    }
+    
     return 0;
 }
