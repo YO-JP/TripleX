b84b5e441e40229d7aa4f5202d4abc474f5e13e1
Yuta Obara <o.yuta722@gmail.com>
2020-02-24 09:48:05 -0500

    final version

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <ctime>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
-    //Print welcome messages to the terminal
-    std::cout << "You are a hacker breaking into NASA's secure server\n";
-    std::cout << "Enter the correct security password to continue...\n\n";
+            //Print welcome messages to the terminal
+    std::cout << "\n\nYou are a hacker breaking into NASA's Level " <<Difficulty;
+    std::cout << " secure server\nEnter the correct security password to continue...\n\n";
 
-    
+}
+
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
+
     //Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     /*
     Declare sum and product variable
@@ -21,9 +27,9 @@ int main()
 
     //Print sum and product to the terminal    
     std::cout << std::endl;
-    std::cout << "There are 3 numbers in the code\n";
-    std::cout << "The codes add-up to: " << CodeSum;
-    std::cout << "\nThe codes multiply to give: " << CodeProduct;
+    std::cout << "+ There are 3 numbers in the code\n";
+    std::cout << "+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct <<std::endl;
 
     //Store player guess
     int GuessA, GuessB, GuessC;
@@ -34,12 +40,34 @@ int main()
 
     if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\nHack: Successful";
+        std::cout << "\nSucess! Keep proceeding to next level";
+        return true;
     }
     else
     {
-        std::cout << "\nDetected! Run!";
+        std::cout << "\n***You entered the wrong code! Try again before FBI detects you!";
+        return false;
+    }
+}
+
+int main()
+{   
+    srand(time(NULL));
+    int LevelDifficulty =1;
+    const int MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;   
+        }
     }
     
+    std::cout << "\n***You have successfully hacked all levels of NASA's secure server***\n";
     return 0;
 }
